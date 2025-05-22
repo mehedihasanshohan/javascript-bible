d00facbf6a86680c399be010f296b5d4043a8e4c
mehedi hasan <mhshohan04@gmail.com>
2025-05-22 10:48:39 +0600

    data fetch with async-await

---
+++ Async-Await/await-fetch.js
@@ -0,0 +1,6 @@
+async function getUsers(){
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts/1')
+  const data = await response.json();
+  console.log(data);
+}
+getUsers();
