9ef5729406ee5c884682c956e7ecf5872332f7b4
mehedi hasan <mhshohan04@gmail.com>
2025-05-25 10:22:44 +0600

    method defination

---
+++ this/method.js
@@ -0,0 +1,12 @@
+let user = {
+  name: "John",
+  age: 30
+};
+
+user.sayHi = function() {
+  alert("Hello!");
+};
+
+user.sayHi(); // Hello!
+
+// A function that is a property of an object is called its method.
