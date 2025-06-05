577fb501d1ec19e212e06ad730e977d48bfbeb34
mehedi hasan <mhshohan04@gmail.com>
2025-06-05 12:33:33 +0600

    hanging comma in properies

---
+++ Objects/the-basics/trailing.js
@@ -0,0 +1,6 @@
+let user = {
+  name: "John",
+  age: 30,
+}
+
+// That is called a “trailing” or “hanging” comma. Makes it easier to add/remove/move around properties, because all lines become alike.
