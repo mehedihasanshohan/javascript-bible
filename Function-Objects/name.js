520b8deb2b6fa9bab78e9b6cbae9801f7b7f23b4
mehedi hasan <mhshohan04@gmail.com>
2025-06-15 17:08:36 +0600

    functions name property

---
+++ Function-Objects/name.js
@@ -0,0 +1,9 @@
+function sayHi() {
+  alert("Hi");
+}
+
+alert(sayHi.name); // sayHi
+
+// All JavaScript functions have a built-in .name property.
+// sayHi.name returns the name of the function as a string.
+
