bfc86e22e5f9c9496cd59910206f9ff71e0365c6
mehedi hasan <mhshohan04@gmail.com>
2025-08-01 16:06:20 +0600

    array task 2 done

---
+++ Array-Mehtods/js-array-task-ph/task2.js
@@ -0,0 +1,10 @@
+// Declare an array of 3 tourist destinations
+const destinations = ['Paris', 'Tokyo', 'New York'];
+// Add a new tourist destination to your tourist array
+destinations.push('Sydney');
+// Add two more to your array
+destinations.push('Rome', 'Cape Town');
+// Remove the last tourist destination you have added
+destinations.pop();
+// display the final array as output
+console.log(destinations); // Output: ['Paris', 'Tokyo', 'New York', 'Sydney', 'Rome']
