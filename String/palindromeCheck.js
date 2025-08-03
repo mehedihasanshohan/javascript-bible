d95f52f3546d0df60cfaf2c4fcef831291237671
mehedi hasan <mhshohan04@gmail.com>
2025-08-03 16:41:31 +0600

    multiple words palindrome check

--- String/palindromeCheck.js
+++ String/palindromeCheck.js
@@ -7,3 +7,33 @@ if (text === reversedText) {
 } else {
     console.log(` ${text}- is not a palindrome word.`);
 }
+
+//  palindrome check using a function
+const isPalindrome = (str) => {
+  const lowerCase = str.toLowerCase();
+  const reversedStr = lowerCase.split('').reverse().join('');
+  if (lowerCase === reversedStr) {
+    console.log("The string is a palindrome.");
+  } else {
+    console.log(` ${str} - is not a palindrome word.`);
+  }
+}
+
+
+
+// multiple words palindrome check using function
+const isArrayPalindrome = (word) => {
+  const lowerCase = word.toLowerCase();
+  const reversedStr = lowerCase.split('').reverse().join('');
+  if (lowerCase === reversedStr) {
+    console.log(`${word} - is a palindrome word.`);
+  } else {
+    console.log(` ${word} - is not a palindrome word.`);
+  }
+}
+
+
+const array = ['shohan', 'rotator', 'level', 'mom', 'dad', 'Mehedi Hasan Shohan'];
+ for (let i = 0; i < array.length; i++) {
+    isArrayPalindrome(array[i]);
+  }
