e1c49ff192e5cea414fc013e9e0a11ca1c1acd47
mehedi hasan <mhshohan04@gmail.com>
2025-08-08 17:06:27 +0600

    calculate avg price from an array of objects

---
+++ Function/ph-problem-solving/calculateAverage.js
@@ -0,0 +1,17 @@
+function average(phones){
+  phnTotalPrice = 0;
+  for(phn of phones){
+    phnTotalPrice += phn.price;
+  }
+    let avg = phnTotalPrice/ phones.length;
+  return avg;
+}
+const phones = [
+        { model: "PhoneA", brand: "Iphone", price: 95000 },
+        { model: "PhoneB", brand: "Samsung", price: 40000 },
+        { model: "PhoneC", brand: "Oppo", price: 26000 },
+        { model: "PhoneD", brand: "Nokia", price: 35000 },
+        { model: "PhoneE", brand: "Iphone", price: 105000 },
+        { model: "PhoneF", brand: "HTC", price: 48000 },
+    ];
+const total = console.log(average(phones));
