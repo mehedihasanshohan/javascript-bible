3d3bd50349e298be93a6035f29f743ecac881a8c
mehedi hasan <mhshohan04@gmail.com>
2025-08-30 11:51:21 +0600

    object destruction basics

--- ES6/destruction/object-destruction/destruction.js
+++ ES6/destruction/object-destruction/destruction.js
@@ -1,3 +1,42 @@
+// basic object destructuring
+// 1. Basic
+const user1 = { name1: "Shohan", age1: 22 };
+const { name1, age1 } = user1;
+console.log(name1, age1); // Shohan 22
+
+// 2. Rename variable
+const { name: userName } = user;
+console.log(userName); // Shohan
+
+// 3. Default value
+const { hobby = "coding" } = user;
+console.log(hobby); // coding
+
+// 4. Nested destructure
+const student = { info: { id: 1, dept: "CSE" } };
+const { info: { id, dept } } = student;
+console.log(id, dept); // 1 CSE
+
+// 5. With rest operator
+const { name: n, ...others } = user;
+console.log(n, others); // Shohan { age: 22 }
+
+// 6. Function parameter destructure
+function printUser({ name, age }) {
+  console.log(`${name} is ${age}`);
+}
+printUser(user); // Shohan is 22
+// 7. Destructure in loop
+const users = [
+  { name: "Shohan", age: 22 },
+  { name: "Rahat", age: 20 },
+];
+for (const { name, age } of users) {
+  console.log(`${name} is ${age}`);
+}
+
+
+// rename variables while destructuring
 const user = { name: "Shohan", age: 22 };
 const { name, age } = user;
 
@@ -6,7 +45,6 @@ console.log(age);  // 22      (variable)
 console.log(user.name); // Shohan  (object key)
 console.log(user.age);  // 22     (object key)
 console.log(user); // { name: "Shohan", age: 22 } (object)
-
 
 //shortcut memory tips:
 // { keyName } → variable name same as key
