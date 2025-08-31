bce67773a0e9e4df646ffab7884c8b0d1604c90e
mehedi hasan <mhshohan04@gmail.com>
2025-08-31 14:56:30 +0600

    find() method

--- ES6/array-methods/find/find.js
+++ ES6/array-methods/find/find.js
@@ -1,3 +1,4 @@
+// ex-1: basic find
 const grillPieces = [10, 20, 30, 40, 50];
 
 // Find the first piece with spice > 25
@@ -6,12 +7,12 @@ const tastyPiece = grillPieces.find(piece => piece > 25);
 console.log(tastyPiece); // 30
 console.log(grillPieces); // Original array remains unchanged: [10, 20, 30, 40, 50]
 
-// Using index and array
+// ex-2: Using index and array
 const names = ['Alice', 'Bob', 'Charlie', 'David'];
 const firstLongName = names.find((name, index, arr) => name.length > 3 && index < arr.length);
 console.log(firstLongName); // 'Alice'
 
-// Finding objects
+// ex-3: Finding objects
 const users = [
   { name: "Shohan", age: 22 },
   { name: "Rahat", age: 18 },
@@ -21,4 +22,4 @@ const adultUser = users.find(user => user.age >= 21);
 console.log(adultUser); // { name: "Shohan", age: 22 }
 // If no match
 const noUser = users.find(user => user.age > 30);
-console.log(noUser); // undefined 
+console.log(noUser); // undefined
