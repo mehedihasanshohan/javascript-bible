5371a00277e0017561e8eb79292361bcb1ff7244
mehedi hasan <mhshohan04@gmail.com>
2025-09-03 17:22:52 +0600

    show post with load more btn and modern ui

--- JSON/json-basic/json.js
+++ JSON/json-basic/json.js
@@ -16,3 +16,15 @@ fetch('https://jsonplaceholder.typicode.com/todos/1')
       .then(response => response.json())
       .then(json => console.log(json));
     // Output: { userId: 1, id: 1, title: 'delectus aut autem', completed: false }
+
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(json => d(json))
+
+const d = (datas) => {
+    datas.forEach(data=>console.log(data))
+}
+// Output: { userId: 1, id: 1, title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit', body: 'quia et suscipit\nsuscipit...
+//         { userId: 1, id: 2, title: 'qui est esse', body: 'est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat...
+//         { userId: 1, id: 3, title: 'ea molestias quasi exercitationem repellat qui ipsa sit aut', body: 'et iusto sed quo iure\nvoluptatem occaecati...
